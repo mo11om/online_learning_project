341d9d082a73e00036127b917ffbe82a466ad9b2
mo1om <morris2001s@yahoo.com.tw>
2022-09-27 17:16:33 +0800

    cost_compute complete

--- env.py
+++ env.py
@@ -2,6 +2,7 @@
 
 
  
+from re import S
 import numpy as np
 import math  
 import random 
@@ -46,44 +47,61 @@ class player :
 
 class all_player :
      def __init__(self,player_num,path_num):
-          self.palyer_list=list()
+          self.player_list=list()
           self.num_player = player_num
           for i in range(player_num):
-               self.palyer_list .append(player(path_num)) 
+               self.player_list .append(player(path_num)) 
+          self.player_list=np.asarray(self.player_list)
 
      def player_print (self):
-          
+           
           for i in range(self.num_player):
-             print(i,  self.palyer_list[i].get_possibility() )
+             print(i,self.player_list[i].get_possibility() )
 
 
 
           
 class path_cost_function:
-     def __init__(self,cost_value):     
-          self.xpower=np.array(cost_value)
+     def __init__(self,cost_value,path_num ):     
+          self.cost_func=list()
+          self.path_num=path_num
+          self.player_cost=list() 
+          for i in cost_value:
+               self.cost_func.append(np.poly1d(i))
+     def get_cost_func (self):
           
-     def get_xpower (self):
-          
-          return self.xpower
-     #def get_cost(self,parameter):
+          return self.cost_func
+     def get_cost(self,players):
+          player_cost= list ()
+          for people in range(players.num_player):
+               tmp = list()
+               for poss in range(self.path_num):
+                    n=self.cost_func[poss](players.player_list[people].possibility[poss] )
+                    
+                    tmp.append(n)
+               self.player_cost.append(tmp)
+          self.player_cost=np.asarray(self.player_cost)
 
+     
 if __name__ == '__main__':
      cost=[[2,3],
           [3,2],
           [4,0]]
-     n1=player(3)
-     # n2=path_cost_function(cost)
-     # res = n1.possibility.dot( n2.xpower)
+     #n1=player(3)
+     n2=path_cost_function(cost,3)
+     #res = n1.possibility.dot( n2.cost_func)
      # # print (n1.get_possibility())
      
      
-     # # print (n2.get_xpower())
+     print(n2.get_cost_func())
+    
 
-
-     # print ("res",res)
+     #print ("res",res)
      n=all_player(6,3)
      n.player_print()
+     n2.get_cost(n)
+     print(n2.player_cost)
+     #n.player_print()
 
 
 
