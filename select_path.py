3c749fc35ff2a639f49964841dafa16f00026430
chi-white <84320035+chi-white@users.noreply.github.com>
2022-10-06 17:15:08 +0800

    Update select_path.py
    strategy_change

--- select_path.py
+++ select_path.py
@@ -6,26 +6,21 @@ def panning(strategy) :
     strategy = strategy + range_
     positive_sum = sum([i for i in strategy if i > 0])
     positive_number = len([i for i in strategy if i > 0])
-    while(positive_sum != 1) :#float problem
+    while  abs(positive_sum - 1) > 0.01  :
         range_ = (1 - positive_sum)/positive_number
         strategy = strategy + range_
         positive_sum = sum([i for i in strategy if i > 0])
         positive_number = len([i for i in strategy if i > 0])
-    return strategy
-
+    strategy[strategy < 0] = 0
+    return strategy 
     
-
-
-# def  refresh_strategy(past_cost , past_strategy, times, learn_rate)
-#      strategy = np.zeros(len(path_strategy))
-#      gradient = np.zeros(len(path_strategy))
-#      for i in range(times): #moving times 
-#         gradient = 2*strategy - 2*past_strategy + past_cost 
-#         strategy = strategy - learn_rate*gradient
-#     sum_ = sum(strategy)
-#     new_strategy = map(strategy) 
-#     return new_stratgy         
-
-
-a = panning(np.array([-1.1, 0.2, 0.4]))
-print(a)
+def  refresh_strategy(past_cost , past_strategy, times, learn_rate) :
+    strategy = np.zeros(len(past_strategy))
+    gradient = np.zeros(len(past_strategy))
+    for i in range(times): #moving times 
+        gradient = past_cost + (2/learn_rate)*(strategy - past_strategy)
+        strategy = strategy - gradient
+        print(strategy)
+    new_strategy = panning(strategy) 
+    return new_strategy
+    #rera   
