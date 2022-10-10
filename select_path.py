fca15fb02f9b217e6cf6ea83e96f12306090be01
chi-white <84320035+chi-white@users.noreply.github.com>
2022-10-10 14:14:51 +0800

    add scale ariable in selecting process

--- select_path.py
+++ select_path.py
@@ -1,26 +1,28 @@
 import numpy as np
-def panning(strategy) :
+def panning(strategy, scale) : #strategy must be a np.array!!
     sum_ = 0
     sum_ = sum(strategy)
-    range_ = 1 - sum_/len(strategy)
+    range_ = scale - sum_/len(strategy)
     strategy = strategy + range_
     positive_sum = sum([i for i in strategy if i > 0])
     positive_number = len([i for i in strategy if i > 0])
-    while  abs(positive_sum - 1) > 0.01  :
-        range_ = (1 - positive_sum)/positive_number
+    while  abs(positive_sum - scale) > 0.01  :
+        range_ = (scale - positive_sum)/positive_number
         strategy = strategy + range_
         positive_sum = sum([i for i in strategy if i > 0])
         positive_number = len([i for i in strategy if i > 0])
     strategy[strategy < 0] = 0
+    strategy = strategy/scale
     return strategy 
     
-def  refresh_strategy(past_cost , past_strategy, times, learn_rate) :
+def  refresh_strategy(past_cost , past_strategy, times, learn_rate, scale) :
     strategy = np.zeros(len(past_strategy))
     gradient = np.zeros(len(past_strategy))
     for i in range(times): #moving times 
         gradient = past_cost + (2/learn_rate)*(strategy - past_strategy)
         strategy = strategy - gradient
-        print(strategy)
-    new_strategy = panning(strategy) 
+    new_strategy = panning(strategy, scale) 
     return new_strategy
-    #rera   
+
+# a = panning(np.array([-0.5, -2.5]), 10)
+# print(a)
