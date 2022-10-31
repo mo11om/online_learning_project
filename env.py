4341108c2e256a03262b62db19a42c59fcd0936a
KimBia9020 <Kim@bshare.io>
2022-10-31 20:31:03 +0800

    thfhtf

--- env.py
+++ env.py
@@ -1,131 +1,153 @@
- 
-
-
- 
- 
- 
-import numpy as np
- 
+import numpy as np 
 import random 
+import select_path
+import copy
 
 class player :
-     def  create_random(self):#romdom crate posssibility
+     def  create_random(self) : #random crate propability
           # print(self.path_num)
-          
-          tmp = np.zeros ((1,self.path_num-1))
-
-    
-          rdtmp =0
-          total_num=self.path_num-1
-          while total_num:
-              
-               
-               rdtmp =      random.randint(1,100)    
-               if(rdtmp  in tmp[0]):
+          tmp = np.zeros ((1, self.path_num-1))
+          rdtmp = 0
+          total_num = self.path_num - 1
+          while total_num :
+               rdtmp = random.randint(1, 100)    
+               if(rdtmp  in tmp[0]) :
                     continue
-               tmp[0][total_num-1]= rdtmp
-                    
-               total_num=total_num-1
-               
-          tmp=np.append(tmp ,np.array( 100))
+               tmp[0][total_num-1] = rdtmp
+               total_num = total_num-1
+          tmp = np.append(tmp ,np.array(100))
           tmp.sort()
-          
-          for i in range(self.path_num-1,0,-1):
-                
-               if i!=0:
-                    tmp [i]= tmp [i]-tmp[i-1]
-               
-          tmp=tmp/100
-           
+          for i in range(self.path_num-1, 0, -1) : 
+               if i != 0:
+                    tmp [i] = tmp [i]-tmp[i-1]            
+          tmp = tmp/100
+          print(tmp)
           return tmp
-     def __init__(self,path_num) :
-          self.path_num=path_num
 
-          self.possibility = np.ones(path_num)
-          self.possibility=self.possibility/path_num
-          #self.create_random()     
-     def get_possibility (self):#[poss1 , poss2, poss3]
-          return self.possibility
+     def __init__(self, path_num) : 
+          self.path_num = path_num
+          self.probability = np.ones(path_num)/path_num
+          self.estimate_probability = np.ones(path_num)/path_num
+          # self.probability = self.create_random()    
+           
+     def get_probability (self) : #[prob1 , prob2, prob3]
+          return self.probability
 
 class all_player :
-     def __init__(self,player_num,path_num):
-          self.player_list=list()
+     def __init__(self, player_num, path_num) :
+          self.players_strategy = list()
+          self.players_estimate = list()
           self.player_num = player_num
-          for i in range(player_num):
-               self.player_list .append(player(path_num)) 
-          self.player_list=np.asarray(self.player_list)
+          for i in range(player_num) : 
+               self.players_strategy.append(player(path_num)) 
+          self.players_strategy = np.asarray(self.players_strategy)
 
-     def player_print (self):
-           
-          for i in range(self.player_num):
-             print(i,self.player_list[i].get_possibility() )
+     def all_player_probability(self) : 
+          for i in range(self.player_num) :
+             print(i, self.players_strategy[i].get_probability())
 
-
-
-          
-class path_cost_function:
-     def __init__(self,cost_value,path_num,player_num ):     
-          self.cost_func=list()
-          self.path_num=path_num
-          self.player_cost=np.zeros(player_num)
-          for i in cost_value:
+class congestion_game(all_player) :
+     def __init__(self, coefficient, path_num, player_num) : 
+          self.cost_func = list()
+          self.path_num = path_num
+          self.path_cost = np.zeros(path_num) 
+          self.total_path_select = dict() #record all player's final choice
+          all_player.__init__(self, player_num=player_num, path_num=path_num)
+          for i in coefficient :
                self.cost_func.append(np.poly1d(i))
-     def get_cost_func (self):
-          
+     
+     def get_cost_func (self) :
           return self.cost_func
-     def random_select_cost(self,players):
-          path=np.zeros(self.path_num)
-
-          
-          total_path_select= {new_list: [] for new_list in range(self.path_num)}
-          for i in range (players.player_num):
-               tmp=np.random.choice(a=3,size=1,p=players.player_list[i].possibility)
-                
-                
-               total_path_select[tmp[0]].append(i)
-          
-
-          print(total_path_select)
-          for key ,value in total_path_select.items():
-               # print(key, type(key),value)
-               tmp_cost=self.cost_func[key](len(value))
-               for player in value:
-                    # print(player,type(player))
-                    # print(tmp_cost)
-                    self.player_cost[player]=tmp_cost       
-
-               
-     def get_cost(self,players):
-           
-          for people in range(players.player_num):
-               tmp = list()
-               for poss in range(self.path_num):
-                    n=self.cost_func[poss](players.player_list[people].possibility[poss] )
-                    
-                    tmp.append(n)
-               self.player_cost.append(tmp)
-          self.player_cost=np.asarray(self.player_cost)
-
      
-if __name__ == '__main__':
-     cost=[[2,3],
-          [3,2],
-          [4,0]]
-     #n1=player(3)
-     n2=path_cost_function(cost,3,6)
-     #res = n1.possibility.dot( n2.cost_func)
-     # # print (n1.get_possibility())
+     def random_select_cost(self) : 
+          self.total_path_select = {new_list: [] for new_list in range(self.path_num)} 
+          #creat empty dict => {0:[], 1:[], 2:[]}
+          for i in range (self.player_num) :
+               choice_path = np.random.choice(
+                   a=self.path_num,
+                   size=1,
+                   p=self.players_strategy[i].estimate_probability)
+               self.total_path_select[choice_path[0]].append(i)
+          print("path distribution : ", self.total_path_select)
+          for path ,driver in self.total_path_select.items() :
+               path_cost = self.cost_func[path](len(driver)) #calculate path cost
+               self.path_cost[path] = path_cost   
+          print("path cost : ", self.path_cost)
+          return self.path_cost
+
+     def update_strategy(self, times, learn_rate, scale) :
+          for i in range(self.player_num) :
+               #different method to update player's strategy
+
+               # self.players_strategy[i].probability = select_path.refresh_strategy(
+               #   self.path_cost, 
+               #   self.players_strategy[i].probability, 
+               #   times, 
+               #   learn_rate, 
+               #   scale)
+
+               self.players_strategy[i].probability = select_path.refresh_strategy_minimize(
+                   self.players_strategy[i].probability,
+                   self.path_cost, 
+                   learn_rate)
+          print("player strategy : ",  self.players_strategy[i].probability) 
+
+     def update_estimate_strategy(self, times, learn_rate, scale) :
+          for i in range(self.player_num) :
+               #different method 
+
+               # self.players_strategy[i].estimate_probability = select_path.refresh_strategy(
+               #     self.path_cost,
+               #     self.players_strategy[i].probability, 
+               #     times, 
+               #     learn_rate, 
+               #     scale)
+
+               self.players_strategy[i].estimate_probability = select_path.refresh_strategy_minimize(
+                   self.players_strategy[i].probability,
+                   self.path_cost, 
+                   learn_rate)          
+          print("player estimate strategy : ",  self.players_strategy[0].estimate_probability) 
      
-     
-     print(n2.get_cost_func())
-    
+     def hindsight(self) :
+          hindsight_real_diff = 0
+          for number in range(self.player_num) :
+               real_path = select_path.get_key(number, self.total_path_select) #本回合實際的路徑
+               real_cost = self.path_cost[real_path]                           #本回合實際的cost
+               hindsight_path  = real_path                                     #後見之明最佳路徑
+               hindsight_cost = 100000000000000000                             #後見之明最低cost
+               exclude_path_select = copy.deepcopy(self.total_path_select)
+               exclude_path_select[real_path].remove(number)
+               for path ,driver in exclude_path_select.items() :
+                    path_cost = self.cost_func[path](len(driver)+1) #calculate path cost
+                    if path_cost < hindsight_cost :
+                         hindsight_cost = path_cost
+                         hindsight_path = path
+               hindsight_real_diff = hindsight_real_diff + (real_cost - hindsight_cost) 
+               #所有人的[真實選擇與後見之明的差異]的總和
+          return hindsight_real_diff           
 
-     #print ("res",res)
-     n=all_player(6,3)
-     n.player_print()
-     n2.random_select_cost(n)
-     print(n2.player_cost)
-     #n.player_print()
-
-
-
+if __name__ == '__main__' :
+     coefficient =[[3,2], [4,1],]
+     player_number = 3
+     path_number = len(coefficient)
+     gradient_times = 100
+     learn_rate = 10
+     T = 20
+     scale = 1
+     hindsight_real_diff = []
+     everage_regret = []
+     potential_value = []
+     game = congestion_game(coefficient, path_number, player_number)
+     for i in range(1, T+1) :
+          print("T : ", i)
+          game.update_estimate_strategy(gradient_times, learn_rate, scale)
+          game.random_select_cost()
+          game.update_strategy(gradient_times, learn_rate, scale)
+          hight = game.hindsight()
+          hindsight_real_diff.append(hight)
+          everage_regret.append(sum(hindsight_real_diff)/i)
+          potential_value.append()##
+     # print(hindsight_real_diff)
+     print(everage_regret)
+     print(select_path.potential_function())
--- select_path.py
+++ select_path.py
@@ -1,26 +1,58 @@
 import numpy as np
-def panning(strategy) :
+from scipy.optimize import minimize
+
+def panning(strategy, scale) : #strategy must be a np.array!!
     sum_ = 0
     sum_ = sum(strategy)
-    range_ = 1 - sum_/len(strategy)
+    range_ = scale - sum_/len(strategy)
     strategy = strategy + range_
     positive_sum = sum([i for i in strategy if i > 0])
     positive_number = len([i for i in strategy if i > 0])
-    while  abs(positive_sum - 1) > 0.01  :
-        range_ = (1 - positive_sum)/positive_number
+    while  abs(positive_sum - scale) > 0.01  :
+        range_ = (scale - positive_sum)/positive_number
         strategy = strategy + range_
         positive_sum = sum([i for i in strategy if i > 0])
         positive_number = len([i for i in strategy if i > 0])
     strategy[strategy < 0] = 0
+    strategy = strategy/scale
     return strategy 
     
-def  refresh_strategy(past_cost , past_strategy, times, learn_rate) :
+def  refresh_strategy(past_cost, past_strategy, times, learn_rate, scale) :
     strategy = np.zeros(len(past_strategy))
     gradient = np.zeros(len(past_strategy))
     for i in range(times): #moving times 
         gradient = past_cost + (2/learn_rate)*(strategy - past_strategy)
         strategy = strategy - gradient
-        print(strategy)
-    new_strategy = panning(strategy) 
+    new_strategy = panning(strategy, scale) 
     return new_strategy
-    #rera   
+
+def object(strategy, last_strategy, cost, learn_rate) :
+    return cost.dot(strategy.T) + (1/learn_rate)*(strategy - last_strategy).dot((strategy - last_strategy).T)
+
+def constraint(strategy) :
+    return np.sum(strategy)- 1
+
+def refresh_strategy_minimize(last_strategy, cost, learn_rate) :
+    x0 = np.full((1, len(last_strategy)), 1/len(last_strategy))
+    b = (0, 1)
+    buds = []
+    for i in range(len(last_strategy)) :
+        buds.append(b)
+    cons = [{'type' : 'eq', 'fun' : constraint}]
+    sol = minimize(fun=object, x0=x0, args=(np.array(last_strategy),
+                   np.array(cost), learn_rate), method='SLSQP', bounds=buds,
+                   constraints=cons)
+    return sol.x
+    
+def get_key(val, total_path_select):
+  for key, value in total_path_select.items():
+    if val in value:
+      return key
+
+def potential_function(total_path_select, cost_func) : #cost_func is a set of poly1d
+    potential_value=0
+    for path, driver in total_path_select.items() :
+        for i in range(0, len(driver)+1) :
+            potential_value = potential_value + cost_func[path](i)
+    return potential_value
+
