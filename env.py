18b9a33f92f88ed44d7a7d55a3111b928d0cb6ab
mo11om <66251276+mo11om@users.noreply.github.com>
2022-12-17 00:30:56 +0800

    delete random

--- env.py
+++ env.py
@@ -5,25 +5,7 @@ import random
  
 
 class player :
-     def  create_random(self) : #random crate propability
-          # print(self.path_num)
-          tmp = np.zeros ((1, self.path_num-1))
-          rdtmp = 0
-          total_num = self.path_num - 1
-          while total_num :
-               rdtmp = random.randint(1, 100)    
-               if(rdtmp  in tmp[0]) :
-                    continue
-               tmp[0][total_num-1] = rdtmp
-               total_num = total_num-1
-          tmp = np.append(tmp ,np.array(100))
-          tmp.sort()
-          for i in range(self.path_num-1, 0, -1) : 
-               if i != 0:
-                    tmp [i] = tmp [i]-tmp[i-1]            
-          tmp = tmp/100
-          print(tmp)
-          return tmp
+     
 
      def __init__(self, path_num) : 
           self.path_num = path_num
