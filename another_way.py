1e794f27a488254e1d0f4db9c7c5356cbaf096f1
mo11om <66251276+mo11om@users.noreply.github.com>
2022-12-16 23:25:35 +0800

    another way object correction

--- another_way.py
+++ another_way.py
@@ -6,14 +6,19 @@ class congestion_game(select_path.congestion_game):
         super().__init__(coefficient, path_num, player_num)
         self.hcost=np.zeros(path_num)
         self.social_cost_plt=[]
+
     def h_cost(self,cost):
         self.hcost[0]=cost[0]*2-2
         self.hcost[1]=cost[1]*2-1
         self.hcost[2]=cost[2]*2-4
         self.hcost[3]=cost[3]*2-0
 
+    # def object(strategy, last_strategy, cost, learn_rate) :
+    #     return cost.dot(strategy.T) + (1/learn_rate)*(strategy - last_strategy).dot((strategy - last_strategy).T)
+    
     def object(strategy, last_strategy, cost, learn_rate) :
-        return cost.dot(strategy.T) + (1/learn_rate)*(strategy - last_strategy).dot((strategy - last_strategy).T)
+        return learn_rate*congestion_game.h_cost(cost).dot(strategy.T) + (strategy - last_strategy).dot((strategy - last_strategy).T)
+
 
     def social_cost(self) :
           total_cost = 0
@@ -21,10 +26,11 @@ class congestion_game(select_path.congestion_game):
                path_cost = self.cost_func[path](len(driver)) #calculate path cost
                total_cost+=path_cost*len(driver)
           return total_cost
+
     def play_a_game(self,T,gradient_times, learn_rate, scale):
         hindsight_real_diff = []
         p_value=[]
-        social_cost_plt = []
+         
         sc=0
         for i in range(1, T+1) :
             print("T : ", i) 
