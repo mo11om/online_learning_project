040fe3392746898870d928f618729aff860053d5
mo11om <66251276+mo11om@users.noreply.github.com>
2022-12-17 00:31:12 +0800

    select  option

--- main.py
+++ main.py
@@ -5,37 +5,66 @@ import select_path
 import another_way
 from plot import plot as plt
 
-def valution():
+def valution(way:str,show:str):
         gradient_times = 10 
         learn_rate = 0.0001
         T = 10 
         scale = 1
         coefficient =[[3,2], [4,1],[2,4],[6,0]]
-        player_number = 10
+        player_number = 20
         path_number = len(coefficient)
 
+       
 
-        game =  select_path. congestion_game(coefficient, path_number, player_number)
+        if  "h" in  way:
+
+                game =  select_path. congestion_game(coefficient, path_number, player_number)
+                
+
+        
+                game.  play_a_game(T,gradient_times, learn_rate, scale)
+
+                if show == "b":
+                
+                        plt.plot_select_path(game.all_play_total_path_select)
+               
         
 
-       
-        game.  play_a_game(T,gradient_times, learn_rate, scale)
+                
+                if show == "l":
 
-         
-         
-        plt.plot_select_path(game.all_play_total_path_select)
-        plt.show()
-     
+                        plt.plot_diff(T=T, real_diff=game.average_regret,line_name="average_regret")
+                        plt.plot_diff(T=T, real_diff=game.potential_value,line_name="potential_value")
 
-       
-       
-        plt.plot_diff(T=T, real_diff=game.average_regret,line_name="average_regret")
-        plt.plot_diff(T=T, real_diff=game.potential_value,line_name="potential_value")
+                 
+        
+        if  "s" in way :
 
-        plt.line_show()
+                game =  another_way. congestion_game(coefficient, path_number, player_number)
+                
+
+        
+                game.  play_a_game(T,gradient_times, learn_rate, scale)
+
+                
+                if "b" in show:
+                
+                        plt.plot_select_path(game.all_play_total_path_select)
+               
+        
+
+                
+                if  "l" in show:
+                
+                        plt.plot_diff(T=T, real_diff=game.social_cost_plt,line_name="social cost")
+                        plt.plot_diff(T , real_diff=game.potential_value,line_name="potential_value")
+
+                
 
 
         
 if __name__ == '__main__' :
-     valution()
+        way = input( "hindcost h social cost s both  hs ")
+        show = input( " bar  b  line  l both bl ")
+        valution(way,show)
      
