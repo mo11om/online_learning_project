4a8b6816e61d0f88aa9251da7bad675df554b64e
mo11om <66251276+mo11om@users.noreply.github.com>
2022-12-18 16:17:16 +0800

    fix hcost

--- another_way.py
+++ another_way.py
@@ -1,26 +1,53 @@
 import select_path
 import numpy as np 
-
+from scipy.optimize import minimize
 class congestion_game(select_path.congestion_game):
     def __init__(self, coefficient, path_num, player_num):
         super().__init__(coefficient, path_num, player_num)
          
         self.social_cost_plt=[]
-
-    def h_cost(self,cost):
-        hcost=np.zeros(self.path_num)
+        
+    def h_cost( cost):
+        hcost=np.zeros(4)
         hcost[0]=cost[0]*2-2
         hcost[1]=cost[1]*2-1
         hcost[2]=cost[2]*2-4
         hcost[3]=cost[3]*2-0
+         
         return hcost
-
-    # def object(strategy, last_strategy, cost, learn_rate) :
-    #     return cost.dot(strategy.T) + (1/learn_rate)*(strategy - last_strategy).dot((strategy - last_strategy).T)
-    
+ 
     def object(strategy, last_strategy, cost, learn_rate) :
         return learn_rate*congestion_game.h_cost(cost).dot(strategy.T) + (strategy - last_strategy).dot((strategy - last_strategy).T)
 
+    def refresh_strategy_minimize(last_strategy, cost, learn_rate) :
+          x0 = np.full((1, len(last_strategy)), 1/len(last_strategy))
+          b = (0, 1)
+          buds = []
+          for i in range(len(last_strategy)) :
+               buds.append(b)
+          cons = [{'type' : 'eq', 'fun' : congestion_game.constraint}]
+          sol = minimize(fun=congestion_game.object, x0=x0, args=(np.array(last_strategy),
+                         np.array(cost), learn_rate), method='SLSQP', bounds=buds,
+                         constraints=cons)
+          return sol.x
+
+    def update_strategy(self, times, learn_rate, scale ) :
+          for i in range(self.player_num) :
+       
+
+               self.players_strategy[i].probability =  congestion_game. refresh_strategy_minimize(
+                   self.players_strategy[i].probability,
+                   self.path_cost, 
+                   learn_rate)
+
+    def update_estimate_strategy(self, times, learn_rate, scale ) :
+          for i in range(self.player_num) :
+               
+
+               self.players_strategy[i].estimate_probability =  congestion_game.refresh_strategy_minimize(
+                   self.players_strategy[i].probability,
+                   self.path_cost, 
+                   learn_rate) 
 
     def social_cost(self) :
           total_cost = 0
